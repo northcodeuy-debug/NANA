f1409fc269b9c8b1a61ec2633bdac9e4969d4096
Ary Gimenez <argi.prog@gmail.com>
2025-09-15 07:40:08 -0300

    Estoy trabjando con el tema de cambiar la foto de portada como pidio la clita

--- src/App.jsx
+++ src/App.jsx
@@ -168,10 +168,23 @@ const App = () => {
 
       
       {/* Sección de la landing page con Three.js */}
+
+      {/* Seccion principal de la web
+          - Resoluciones recomendadas:
+            - Móviles: 375px - 667px
+            - Tablets: 768px - 1024px
+            - Escritorio: 1280px - 1920px
+          - Recomendaciones de Geminis
+            1920x1080 - 2560x1440 - 16:9
+          Metodos de compresion TinyPNG
+           - No pear mas de 200-300 KB.
+        */}
+
       <div className="relative w-screen h-screen overflow-hidden flex flex-col items-center justify-center text-center">
         {/* El canvas de Three.js ocupa toda la pantalla */}
+       
+        {/* Cuadro Nana */}
         <div ref={landingCanvasRef} className="absolute inset-0 z-0"></div>
-        
         
         <div className="relative z-10 p-8 rounded-xl bg-[var(--color-primary)]/50 backdrop-blur-md shadow-2xl">
           <h1 className="text-text-primary text-5xl md:text-7xl font-extrabold mb-4 font-brand">#NANA</h1>
@@ -179,9 +192,6 @@ const App = () => {
             Ropa que define tu estilo.
           </p>
         </div>
-      
-      
-      
       </div>
       
         
